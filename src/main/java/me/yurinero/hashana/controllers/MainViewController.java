3d24ac3e230fadfff4a89bd05657bea23ff9a53d
Yurinero <joebla101@gmail.com>
2025-05-29 21:07:46 +0200

    Created custom window styling for the settings-view.fxml by re-using some MainViewController code in SettingsController -> might move into Util class later Created method to open the actual Settings window in MainViewController Added further fx:id references to settings-view.fxml

--- src/main/java/me/yurinero/hashana/controllers/MainViewController.java
+++ src/main/java/me/yurinero/hashana/controllers/MainViewController.java
@@ -1,10 +1,20 @@
 package me.yurinero.hashana.controllers;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -30,6 +40,7 @@ public class MainViewController implements Initializable {
 	public void initialize(URL url, ResourceBundle resourceBundle) {
 	setupWindowControls();
 	setupDragging();
+	settingsButton.setOnAction(this::openSettings);
 	}
 
 
@@ -64,7 +75,37 @@ public class MainViewController implements Initializable {
 			stage.setY(event.getScreenY() - yOffset);
 		});
 	}
+	@FXML
+	private void openSettings(ActionEvent event) {
+		try {
+			// Load the settings FXML file
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/me/yurinero/hashana/settings-view.fxml"));
+			Parent root = loader.load();
 
+			// Create new stage for settings window
+			Stage settingsStage = new Stage();
+			// Load the relevant controller
+			SettingsController controller = loader.getController();
+			// Pass the stage to the controller
+			controller.setStage(settingsStage);
+			settingsStage.setTitle("Settings");
+			settingsStage.setScene(new Scene(root));
+
+			// Set modality to block main window interaction
+			settingsStage.initModality(Modality.APPLICATION_MODAL);
+			// Disable system default window styling
+			settingsStage.initStyle(StageStyle.UNDECORATED);
+			// Set owner to link windows
+			Stage mainStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+			settingsStage.initOwner(mainStage);
+			// Show the result
+			settingsStage.show();
+		} catch (IOException e) {
+			System.err.println("Error loading settings window:");
+			e.printStackTrace();
+
+		}
+	}
 
 	// Setup for window maximization, currently unused  but here if needed to be implemented.
 	/*
--- src/main/java/me/yurinero/hashana/controllers/SettingsController.java
+++ src/main/java/me/yurinero/hashana/controllers/SettingsController.java
@@ -1,16 +1,55 @@
 package me.yurinero.hashana.controllers;
 
+import javafx.application.Platform;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
 public class SettingsController {
 	public TextField bufferSize;
 	public TextField maxFileSize;
 	public TextField progressIntervalMS;
 	public CheckBox splashScreenEnabled;
+	public HBox settingsTitle;
+	public Button settingsApply;
+	public Button settingsSave;
+	public Button settingsClose;
+
+	private Stage stage;
+	private double xOffset = 0;
+	private double yOffset = 0;
 
 	public void initialize() {
+	setupDragging();
+	setupWindowControls();
+	}
+	public void setStage(Stage stage) {
+		this.stage = stage;
+	}
+
+	private void setupWindowControls(){
+		// Close button action
+		settingsClose.setOnAction(event -> {
+			stage.close();
+		});
 
 	}
 
+	// Re-using code, might move into util class at some point.
+	private void setupDragging() {
+		// Capture initial mouse position relative to the stage
+		settingsTitle.setOnMousePressed(event -> {
+
+			xOffset = event.getSceneX();
+			yOffset = event.getSceneY();
+		});
+		// Move the stage using the initial offsets
+		settingsTitle.setOnMouseDragged(event -> {
+
+			stage.setX(event.getScreenX() - xOffset);
+			stage.setY(event.getScreenY() - yOffset);
+		});
+	}
 }
