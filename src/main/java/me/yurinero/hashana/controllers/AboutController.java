5f4abee83b04f5a69bde825aa8b6e315219ed1f1
Yurinero <joebla101@gmail.com>
2025-06-11 18:58:47 +0200

    Start if About Tab implementation, added Text Field to hold information, mascot image, version label, created relevant controller and modified main-view.fxml to include new about-view.fxml

---
+++ src/main/java/me/yurinero/hashana/controllers/AboutController.java
@@ -0,0 +1,11 @@
+package me.yurinero.hashana.controllers;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AboutController {
+
+	public TextField aboutText;
+	public Label versionInfoLabel;
+
+}
